267b561c4e95b1ed35f859bc5e96f81bc9a3999b
Gergo Gyori <gergo.gyori@gmail.com>
2022-05-23 12:09:21 +0200

    minor bug fix, labels combinations saved into an array

--- trainRNN.py
+++ trainRNN.py
@@ -5,12 +5,11 @@ import functions as f
 import preprocessing as pp
 import neuralnetworks as nn
 
-def trainRNN(data_sets, y_train, y_test, early_stop_patience=2, filename="train"):
+def trainRNN(data_sets, y_train, y_test, early_stop_patience=2, filename="train", maxlen=40):
     ### INIT RESULTS
     results, time = f.init_log_for_training()
     # Sentencies max length:
 
-    max_len = 40 # maximum length of sentencies
     # its 2 FOR NORMAL CASE
     batch_size = 25 # bigger = faster train but super unaccurate, lower = slower. Its usually 50
 
@@ -24,19 +23,19 @@ def trainRNN(data_sets, y_train, y_test, early_stop_patience=2, filename="train"
         Train = tokenizer.texts_to_sequences(data_set[1])
         Test = tokenizer.texts_to_sequences(data_set[2])
             # Sequencer 
-        X_train_p = pp.sequence_pad(Train, maxlen=max_len) # there are several attributes which can be defined, basic = first 50 words 
-        X_test_p = pp.sequence_pad(Test,maxlen=max_len)
+        X_train_p = pp.sequence_pad(Train, maxlen=maxlen) # there are several attributes which can be defined, basic = first 50 words 
+        X_test_p = pp.sequence_pad(Test,maxlen=maxlen)
 
             # TRAIN
         #print("shapes: ", X_train_p.shape, X_test_p.shape)
         history, model = nn.RNN_train(X_train_p, y_train, X_test_p, y_test, tokenizer, 
-                                      maxlen=max_len, early_stop_patience=early_stop_patience, batch_size=batch_size)
+                                      maxlen=maxlen, early_stop_patience=early_stop_patience, batch_size=batch_size)
         ### LOGGING INIT - RNN
         # GET the index of the highest test ACCURACY where the RNN model stopped to TRAIN
         max_value = max(history.history['val_accuracy'])
         max_index = history.history['val_accuracy'].index(max_value)
         # How many percent of the sentencies were catched full
-        ratio_train, ratio_test = f.lengths_catch(data_set[1], data_set[2], length_of_sentence=max_len)
+        ratio_train, ratio_test = f.lengths_catch(data_set[1], data_set[2], length_of_sentence=maxlen)
 
         new_row = {'Running ID':time, 
                "Model Name":"RNN", 
@@ -53,8 +52,9 @@ def trainRNN(data_sets, y_train, y_test, early_stop_patience=2, filename="train"
               "Test Loss STOP": history.history['val_loss'][max_index],
                   "Train_sentence_fully_catched_ratio": ratio_train,
                    "Test_sentence_fully_catched_ratio": ratio_test,
-                   "Length of Sentence":max_len,
-                  "Batch size of RNN":batch_size}
+                   "Length of Sentence":maxlen,
+                  "Batch size of RNN":batch_size,
+                  "label_array": str(labels)}
 
         results = results.append(new_row, ignore_index=True)
         # maybe we dont need it in every round but how knows
